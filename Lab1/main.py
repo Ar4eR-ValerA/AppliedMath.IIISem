6f7b6bcb9640ce908c0e1b2392c14591dbe411a6
Valery Shevchenko <ar4er.valera@gmail.com>
2022-03-23 11:50:09 +0300

    feat: GoldenRatioAlgorithm

--- Lab1/main.py
+++ Lab1/main.py
@@ -21,4 +21,28 @@ def DichotomyAlgorithm(leftBound, rightBound, eps):
 
     return segments
 
+def GoldenRatioAlgorithm(leftBound, rightBound, eps):
+    goldenRatio = (math.sqrt(5) + 1) / 2
+    segments = []
+
+    while rightBound - leftBound > eps:
+        segments.append((leftBound, rightBound))
+
+        leftPoint = rightBound - (rightBound - leftBound) / goldenRatio
+        rightPoint = leftBound + (rightBound - leftBound) / goldenRatio
+
+        leftResult = Function(leftPoint)
+        rightResult = Function(rightPoint)
+
+        if leftResult < rightResult:
+            rightBound = rightPoint
+        else:
+            leftBound = leftPoint
+
+    return segments
+
+print("DichotomyAlgorithm:")
 print(DichotomyAlgorithm(3, 6, 1e-8))
+
+print("GoldenRatioAlgorithm")
+print(GoldenRatioAlgorithm(3, 6, 1e-8))
