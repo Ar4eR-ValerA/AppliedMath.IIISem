bc65435a1db2ee640646b526a77cb9ffd6205709
Valery Shevchenko <ar4er.valera@gmail.com>
2022-03-23 12:12:40 +0300

    feat: FibonacciAlgorithm

--- Lab1/main.py
+++ Lab1/main.py
@@ -19,6 +19,7 @@ def DichotomyAlgorithm(leftBound, rightBound, eps):
         else:
             leftBound = middle
 
+    segments.append((rightBound + leftBound) / 2)
     return segments
 
 def GoldenRatioAlgorithm(leftBound, rightBound, eps):
@@ -39,10 +40,70 @@ def GoldenRatioAlgorithm(leftBound, rightBound, eps):
         else:
             leftBound = leftPoint
 
+    segments.append((rightBound + leftBound) / 2)
     return segments
 
-print("DichotomyAlgorithm:")
-print(DichotomyAlgorithm(3, 6, 1e-8))
+def GetFibonacciNumber(n):
+    return -1 / math.sqrt(5) * pow((1 - math.sqrt(5)) / 2, n) + 1 / math.sqrt(5) * pow((1 + math.sqrt(5)) / 2, n)
 
-print("GoldenRatioAlgorithm")
-print(GoldenRatioAlgorithm(3, 6, 1e-8))
+def FibonacciAlgorithm(leftBound, rightBound, eps):
+    segments = []
+
+    iterationNumber = 1
+    fibonacciNumber_n = 1
+    while fibonacciNumber_n <= (rightBound - leftBound) / eps:
+        iterationNumber += 1
+        fibonacciNumber_n = GetFibonacciNumber(iterationNumber)
+    
+    fibonacciNumber_n_minus_1 = GetFibonacciNumber(iterationNumber - 1)
+    fibonacciNumber_n_minus_2 = fibonacciNumber_n - fibonacciNumber_n_minus_1
+
+    leftPoint = leftBound + (rightBound - leftBound) * fibonacciNumber_n_minus_2 / fibonacciNumber_n
+    rightPoint = leftBound + (rightBound - leftBound) * fibonacciNumber_n_minus_1 / fibonacciNumber_n
+
+    leftResult = Function(leftPoint)
+    rightResult = Function(rightPoint)
+
+    for i in range(iterationNumber, 1, -1):
+        segments.append((min(leftBound, rightBound), max(leftBound, rightBound)))
+
+        if abs(rightBound - leftBound) < eps:
+            segments.append((max(leftBound, rightBound) + min(leftBound, rightBound)) / 2)
+            return segments
+
+        if leftResult < rightResult:
+            rightBound = rightPoint
+
+            rightPoint = leftPoint
+            rightResult = leftResult
+
+            leftPoint = leftBound + (rightBound - rightPoint)
+            leftResult = Function(leftPoint)
+        else:
+            leftBound = leftPoint
+
+            leftPoint = rightPoint
+            leftResult = rightResult
+
+            rightPoint = rightBound + (leftBound - leftPoint)
+            rightResult = Function(rightPoint)
+
+    segments.append((max(leftBound, rightBound) + min(leftBound, rightBound)) / 2)
+    return segments
+
+
+
+print("DichotomyAlgorithm:")
+segments = DichotomyAlgorithm(3, 6, 1e-8)
+print(len(segments), segments[len(segments) - 1])
+print(segments, "\n")
+
+print("GoldenRatioAlgorithm:")
+segments = GoldenRatioAlgorithm(3, 6, 1e-8)
+print(len(segments), segments[len(segments) - 1])
+print(segments, "\n")
+
+print("FibonacciAlgorithm:")
+segments = FibonacciAlgorithm(3, 6, 1e-8)
+print(len(segments), segments[len(segments) - 1])
+print(segments, "\n")
