dde3f1a9285adeb2eb242e22990940977f2ffe19
Ar4eR-ValerA <ar4er.valera@gmail.com>
2022-03-24 11:59:28 +0300

    upgrade Brent

--- Lab1/algorithms/brent_algorithm.py
+++ Lab1/algorithms/brent_algorithm.py
@@ -26,25 +26,32 @@ def find_min(function, left_bound, right_bound, eps):
     left_result = right_result = middle_result
     prev_len = current_len = right_bound - left_bound
     segments = []
+    inner_point = middle
     eps /= 10
     calls = 1
 
     while fabs(right_bound - left_bound) >= 10 * eps:
-        segments.append([min(left_bound, right_bound), max(left_bound, right_bound)])
         prev_prev_len = prev_len
         prev_len = current_len
-        parabola_min = 0
-        parabola_fl = False
+        parabola_min = None
 
         if fabs(left_point - middle) > eps or fabs(middle - right_point) > eps:
-            parabola_min = find_parabola_min(function, left_point, middle, right_point)
+            parabola_min = find_parabola_min(function, left_bound, middle, right_bound)
             calls += 3
-            parabola_fl = True
 
-        if (parabola_fl and parabola_min is not None and fabs(parabola_min - left_point) < eps and fabs(right_point - parabola_min) < eps
+        if (parabola_min is not None and fabs(parabola_min - left_bound) > eps and fabs(right_bound - parabola_min) > eps
                 and fabs(parabola_min - middle) < prev_prev_len / 2):
-            inner_point = parabola_min
+
             current_len = fabs(inner_point - middle)
+            if left_bound < parabola_min < inner_point:
+                right_bound = inner_point
+                inner_point = parabola_min
+                segments.append((inner_point, right_bound))
+
+            elif inner_point < parabola_min < right_bound:
+                left_bound = inner_point
+                inner_point = parabola_min
+                segments.append((left_bound, inner_point))
 
         else:
             if middle <= (right_bound + left_bound) / 2:
@@ -69,6 +76,7 @@ def find_min(function, left_bound, right_bound, eps):
 
                 right_point, left_point, middle = left_point, middle, inner_point
                 right_result, left_result, middle_result = left_result, middle_result, inner_result
+                segments.append([min(left_bound, right_bound), max(left_bound, right_bound)])
 
             else:
                 if inner_point >= middle:
@@ -79,9 +87,11 @@ def find_min(function, left_bound, right_bound, eps):
                 if inner_result <= left_result or left_point == middle:
                     right_point, left_point = left_point, inner_point
                     right_result, left_result = left_result, inner_result
+                    segments.append([min(left_bound, right_bound), max(left_bound, right_bound)])
 
                 elif inner_result <= right_result or right_point == middle:
                     right_point, right_result = inner_point, middle_result
+                    segments.append([min(left_bound, right_bound), max(left_bound, right_bound)])
 
     segments.append([min(left_bound, right_bound), max(left_bound, right_bound)])
     return (segments[-1][0] + segments[-1][1]) / 2, calls, segments
