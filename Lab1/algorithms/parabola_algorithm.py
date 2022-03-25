5b89a4068f9a4fc0ad8b2660c203d049dd864fea
Valery Shevchenko <ar4er.valera@gmail.com>
2022-03-25 08:00:56 +0300

    optimize parabola func

--- Lab1/algorithms/parabola_algorithm.py
+++ Lab1/algorithms/parabola_algorithm.py
@@ -3,32 +3,38 @@ def find_min(function, left_bound, right_bound, eps):
     calls = 0
     middle = (right_bound + left_bound) / 2
 
+    left_result = function(left_bound)
+    middle_result = function(middle)
+    right_result = function(right_bound)
+    calls += 3
+
     while abs(right_bound - left_bound) > eps:
         segments.append((left_bound, right_bound))
-
-        left_result = function(left_bound)
-        middle_result = function(middle)
-        right_result = function(right_bound)
-        calls += 3
 
         if left_result < middle_result:
             right_bound = middle
             middle = (left_bound - right_bound) / 2
+
+            right_result = middle_result
+            middle_result = function(middle)
+            calls += 1
         elif right_result < middle_result:
             left_bound = middle
             middle = (left_bound - right_bound) / 2
+
+            left_result = middle_result
+            middle_result = function(middle)
+            calls += 1
         else:
             break
 
     left_point = left_bound
     inner_point = middle
     right_point = right_bound
-    while inner_point - left_point > eps and right_point - inner_point > eps:
 
-        left_result = function(left_point)
-        inner_result = function(inner_point)
-        right_result = function(right_point)
-        calls += 3
+    inner_result = middle_result
+
+    while inner_point - left_point > eps and right_point - inner_point > eps:
 
         a1 = (inner_result - left_result) / (inner_point - left_point)
         a2 = 1 / (right_point - inner_point) * ((right_result - left_result) / (right_point - left_point) -
@@ -39,11 +45,21 @@ def find_min(function, left_bound, right_bound, eps):
         if left_point < min_point < inner_point:
             right_point = inner_point
             inner_point = min_point
+
+            right_result = inner_result
+            inner_result = function(inner_point)
+            calls += 1
+
             segments.append((inner_point, right_point))
 
         elif inner_point < min_point < right_point:
             left_point = inner_point
             inner_point = min_point
+
+            left_result = inner_result
+            inner_result = function(inner_point)
+            calls += 1
+
             segments.append((left_point, inner_point))
 
         else:
