8891d155663c4485c457f08accb4893145db429a
Andreev Artem <andreev_3731@mail.ru>
2022-04-20 14:29:53 +0300

    fix: some errors

--- Lab2/oracle/testOracles/oracle2.py
+++ Lab2/oracle/testOracles/oracle2.py
@@ -1,5 +1,3 @@
-import math
-
 import numpy as np
 from oracle.firstOrderOracle import FirstOrderOracle
 
@@ -7,8 +5,8 @@ from oracle.firstOrderOracle import FirstOrderOracle
 class Oracle2(FirstOrderOracle):
 
     def function(self, arguments):
-        return arguments[0] * math.exp(-(arguments[0] ** 2 + arguments[1] ** 2))
+        return arguments[0] * np.exp(-(arguments[0] ** 2 + arguments[1] ** 2))
 
     def gradient(self, arguments):
-        return np.array([(1 - 2 * (arguments[0] ** 2)) * math.exp(-arguments[0] ** 2 - arguments[1] ** 2),
-                         -2 * arguments[0] * arguments[1] * math.exp(-arguments[0] ** 2 - arguments[1] ** 2)])
+        return np.array([(1 - 2 * (arguments[0] ** 2)) * np.exp(-arguments[0] ** 2 - arguments[1] ** 2),
+                         -2 * arguments[0] * arguments[1] * np.exp(-arguments[0] ** 2 - arguments[1] ** 2)])
