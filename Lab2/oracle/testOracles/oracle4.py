9222c14887304b9066c43186c920be7f7080c724
Valery Shevchenko <ar4er.valera@gmail.com>
2022-04-20 21:03:09 +0300

    feat: new o4

--- Lab2/oracle/testOracles/oracle4.py
+++ Lab2/oracle/testOracles/oracle4.py
@@ -5,7 +5,18 @@ from oracle.firstOrderOracle import FirstOrderOracle
 class Oracle4(FirstOrderOracle):
 
     def function(self, arguments):
-        return arguments[0] ** 2 + arguments[0] * (arguments[1] + 4) + 5 * (arguments[1] - 3) ** 2
+        x = arguments[0]
+        y = arguments[1]
+        return np.log(10000 * (x + 1) ** 8 + x ** 4 * y ** 4 + y ** 8 + 0.1) + 2
 
     def gradient(self, arguments):
-        return np.array([2 * arguments[0] + arguments[1] + 4, arguments[0] + 10 * (arguments[1] - 3)])
+        x = arguments[0]
+        y = arguments[1]
+
+
+        t = np.log10(x ** 4 * y ** 4 + 10000 * (x + 1) ** 8 + y ** 8 + 0.1)
+
+        g1 = (4 * x ** 3 * y ** 4 + 80000 * (x + 1) ** 7) / t
+        g2 = (4 * y ** 3 * (x ** 4 + 2 * y ** 4)) / t
+
+        return np.array([g1, g2])
