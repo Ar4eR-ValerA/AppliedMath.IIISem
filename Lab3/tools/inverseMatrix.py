f2e91db7b707493106ea006dbbdce65f6c517216
Valery Shevchenko <ar4er.valera@gmail.com>
2022-05-14 16:22:58 +0300

    chore: inverse matrix    upgrade with separate solving

--- Lab3/tools/inverseMatrix.py
+++ Lab3/tools/inverseMatrix.py
@@ -7,8 +7,8 @@ def inverse_matrix(a: np.array):
     e = get_identity_matrix(n)
     l, u = lu_decomposition(a)
 
-    t = np.linalg.solve(l, e)
-    return np.linalg.solve(u, t)
+    t = linalg_smart_solve(l, e)
+    return linalg_smart_solve(u, t)
 
 
 def get_identity_matrix(n: int):
@@ -21,3 +21,17 @@ def get_identity_matrix(n: int):
         matrix.append(list)
 
     return np.array(matrix)
+
+
+def linalg_smart_solve(a: np.array, v: np.array):
+    x = []
+    n = len(a)
+    a = a
+    v = v.transpose()
+
+    for i in range(0, n):
+        ans_vector = np.linalg.solve(a, v[i])
+        x.append(ans_vector)
+
+    matrix_x = np.array(x)
+    return matrix_x.transpose()
