f2f90e04425d729ca3621e2a1b6b51722ffb2d2e
Valery Shevchenko <ar4er.valera@gmail.com>
2022-05-18 09:45:51 +0300

    feat: lil matrix

--- Lab3/tools/linearEquationsSystemSolve.py
+++ Lab3/tools/linearEquationsSystemSolve.py
@@ -1,16 +1,14 @@
-import numpy as np
 from scipy import sparse
+from scipy.sparse import linalg
 
 
-def linear_equations_system_solve(a: sparse.csr_matrix, v: sparse.csr_matrix):
+def linear_equations_system_solve(a: sparse.lil_matrix, v: sparse.lil_matrix):
     x = []
     n = a.shape[0]
-    a = a.toarray()
-    v = v.toarray().transpose()
 
     for i in range(0, n):
-        ans_vector = np.linalg.solve(a, v[i])
+        ans_vector = linalg.spsolve(a, v.getcol(i))
         x.append(ans_vector)
 
-    matrix_x = sparse.csr_matrix(x)
+    matrix_x = sparse.lil_matrix(x)
     return matrix_x.transpose()
--- Lab3/tools/luDecomposition.py
+++ Lab3/tools/luDecomposition.py
@@ -1,12 +1,10 @@
-import numpy as np
 from scipy import sparse
 
 
-def lu_decomposition(a: sparse.csr_matrix):
+def lu_decomposition(a: sparse.lil_matrix):
     n = a.shape[0]
-    # TODO: Мне кажется, что переводить из CSR в обычную матрицу - это ПЛОХО!
-    # TODO: Нужно придумать, как приспособить три написанных алгоритма под CSR
-    u = a.toarray()
+
+    u = a
     list_l = []
 
     for i in range(0, n):
@@ -17,19 +15,19 @@ def lu_decomposition(a: sparse.csr_matrix):
         temp_l[i] = 1
         list_l.append(temp_l)
 
-    l = np.array(list_l)
+    l = sparse.lil_matrix(list_l)
 
     for i in range(0, n):
         for j in range(i, n):
-            l[j][i] = u[j][i] / u[i][i]
+            l[j, i] = u[j, i] / u[i, i]
 
     for k in range(1, n):
         for i in range(k - 1, n):
             for j in range(i, n):
-                l[j][i] = u[j][i] / u[i][i]
+                l[j, i] = u[j, i] / u[i, i]
 
         for i in range(k, n):
             for j in range(k - 1, n):
-                u[i][j] = u[i][j] - l[i][k-1] * u[k - 1][j]
+                u[i, j] = u[i, j] - l[i, k-1] * u[k - 1, j]
 
     return sparse.csr_matrix(l), sparse.csr_matrix(u)
