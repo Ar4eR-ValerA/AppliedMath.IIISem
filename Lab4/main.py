15e0e29f8849b41c599a261f02a475d21dcc1aa0
Andreev Artem <andreev_3731@mail.ru>
2022-06-03 19:15:14 +0300

    feat: add eigenvectors check

--- Lab4/main.py
+++ Lab4/main.py
@@ -14,6 +14,14 @@ print(vec)
 print("\nКол-во итераций:")
 print(iters)
 
+print("\nМатрица A * собственный вектор №1:", np.dot(A, vec.transpose()[0]))
+print("Собственное число №1 * собственный вектор №1:", np.dot(ans[0], vec.transpose()[0]))
+
+print("\nМатрица A * собственный вектор №2:", np.dot(A, vec.transpose()[1]))
+print("Собственное число №2 * собственный вектор №2:", np.dot(ans[1], vec.transpose()[1]))
+
+print("\nМатрица A * собственный вектор №3:", np.dot(A, vec.transpose()[2]))
+print("Собственное число №3 * собственный вектор №3", np.dot(ans[2], vec.transpose()[2]))
 
 print("______________________________________\n")
 B = np.array([[5, 1, 2], [1, 4, 1], [2, 1, 3]])
@@ -26,3 +34,12 @@ print("\nСобственные векторы:")
 print(vec)
 print("\nКол-во итераций:")
 print(iters)
+
+print("\nМатрица B * собственный вектор №1:", np.dot(B, vec.transpose()[0]))
+print("Собственное число №1 * собственный вектор №1:", np.dot(ans[0], vec.transpose()[0]))
+
+print("\nМатрица B * собственный вектор №2:", np.dot(B, vec.transpose()[1]))
+print("Собственное число №2 * собственный вектор №2:", np.dot(ans[1], vec.transpose()[1]))
+
+print("\nМатрица B * собственный вектор №3:", np.dot(B, vec.transpose()[2]))
+print("Собственное число №3 * собственный вектор №3:", np.dot(ans[2], vec.transpose()[2]))
